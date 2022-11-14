db165585abe088a3152d06dbcc7e57f104eefb32
unknown <arthurguilherme2013@gmail.com>
2022-11-14 13:22:12 +0000

    Criação de Objetos e Entendendo Propriedades

---
+++ 2_Objeto/1_CriarObjeto/js/script.js
@@ -0,0 +1,7 @@
+let carro = {
+    portas: 4,
+    cor: "red"
+}
+
+console.log(carro.portas)
+console.log(carro["cor"])
---
+++ 2_Objeto/2_propriedades/js/script.js
@@ -0,0 +1,11 @@
+let carro = {
+    portas: 4,
+    cor: "red"
+}
+
+console.log(carro.portas)
+console.log(carro["cor"])
+
+if(carro.portas > 2) {
+    console.log("Este carro tem mais que duas portas!")
+}

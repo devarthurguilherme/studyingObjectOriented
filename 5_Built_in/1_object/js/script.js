6cfe0e1339cf315000b7ff872c5e0376f5a5019c
unknown <arthurguilherme2013@gmail.com>
2022-12-15 06:34:51 +0000

    Studying basic conceps about the Built in: Objects ,Arrays

---
+++ 5_Built_in/1_object/js/script.js
@@ -0,0 +1,30 @@
+let a = [0, 1, 2];
+let b = new Array(3, 4, 5);
+/*console.log(a);
+console.log(b);
+
+console.log(a.toString())
+console.log(b.toString())
+
+console.log(a instanceof Array)
+console.log(b instanceof Array)
+console.log(a instanceof Object)
+console.log(b instanceof Object)
+console.log(Array instanceof Object)*/
+
+console.log(a.length)
+console.log(b.length)
+
+a.push(4)
+b.push(5)
+
+console.log(a)
+console.log(b)
+
+a.pop()
+b.pop()
+
+console.log(a)
+console.log(b)
+
+
---
+++ 5_Built_in/2_array/js/script.js
@@ -0,0 +1,11 @@
+let o1 = {};
+let o2 = new Object();
+
+console.log(o1)
+console.log(o2)
+
+o1.nome = "Arthur";
+o2.nome = "Guilherme";
+
+console.log(o1.valueOf())
+console.log(o2.valueOf())

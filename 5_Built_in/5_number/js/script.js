dd6e70f991f7e6342653e4cc0c775652d9ac4b92
unknown <arthurguilherme2013@gmail.com>
2022-12-20 06:36:15 +0000

    Estudando Numbers e Strings

---
+++ 5_Built_in/5_number/js/script.js
@@ -0,0 +1,19 @@
+/*let a = 2;
+let b = new Number(3);
+let c = 12.48
+let d = "1992"
+
+console.log(a);
+console.log(b);
+console.log(parseInt(c));
+console.log(parseFloat(d))
+
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_VALUE)
+console.log(Number.NaN)*/
+
+let f1 = 3.1421364;
+console.log(f1)
+console.log(f1.toFixed(3))
+console.log(f1.toPrecision(4))
+console.log(f1.toExponential())
---
+++ 5_Built_in/6_string/js/script.js
@@ -0,0 +1,7 @@
+let s1 = new String("Arthur");
+let s2 = "Guilherme";
+
+console.log(s1.length);
+console.log(s2.length);
+console.log(s1[3]);
+console.log(s2[3]);
